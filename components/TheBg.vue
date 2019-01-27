56a6af80407a9c911fba9bec1f7ea290cd7732e8
mkobayashime <m.kobayashi.me@gmail.com>
2019-01-27 16:14:21 +0900

    Add BG and Sidebar components

---
+++ components/TheBg.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+	#the-bg
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style lang="stylus" scoped>
+#the-bg
+	position absolute
+	width 100%
+	height 100%
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,18 @@
 <template lang="pug">
   #default
+    the-bg
+    the-sidebar
     nuxt
 </template>
+
+<script>
+export default {
+  components: {
+    TheBg: () => import('~/components/TheBg.vue'),
+    TheSidebar: () => import('~/components/TheSidebar.vue')
+  }
+}
+</script>
 
 <style>
 </style>
