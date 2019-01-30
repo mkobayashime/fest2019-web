d0fd51015107f7f41c21cabf5687d783f643f17f
mkobayashime <m.kobayashi.me@gmail.com>
2019-01-31 00:39:56 +0900

    Add micro interactions to sidebar

---
+++ components/TheMenu.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+	#the-menu
+		img.menuIcon(src="~/assets/icon/menu.svg")
+</template>
+
+<style lang="stylus" scoped>
+#the-menu
+	position absolute
+	display flex
+	align-items flex-start
+	justify-content center
+	width 70px
+	height 100px
+	background-color #000
+	transition all 200ms ease-out
+	&:hover
+		height 150px
+	.menuIcon
+		width 25px
+		height 25px
+		margin 37.5px 0 0 0
+</style>
---
+++ components/TheSidebar.vue
@@ -0,0 +1,96 @@
+<template lang="pug">
+	#the-sidebar
+		the-menu
+		.sidebar-items
+			.items-group
+				.links
+					a(:href="sns.twitter" target="_blank")
+						.sidebar-item.fab.fa-twitter
+					a(:href="sns.ig" target="_blank")
+						.sidebar-item.fab.fa-instagram
+					a(:href="sns.fb" target="_blank")
+						.sidebar-item.fab.fa-facebook
+				p Follow Us
+			.items-group
+				.links
+					a(:href="sns.twitter" target="_blank")
+						.sidebar-item.fab.fa-twitter
+					a(:href="sns.facebook" target="_blank")
+						.sidebar-item.fab.fa-facebook
+				p Share On
+</template>
+
+<script>
+import sns from '~/assets/data/sns.json'
+export default {
+  components: {
+    TheMenu: () => import('~/components/TheMenu.vue')
+  },
+  data() {
+    return {
+      sns: sns
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+#the-sidebar
+	width 70px
+	height 100vh
+	transition all 200ms ease-out
+	/* &:hover
+		& .items-group
+			margin 0 0 30px 0
+		& .links
+			opacity 1
+	.items-group
+		margin 0 40px 30px 0
+		transition all 200ms ease-out
+	.links
+		opacity 0
+		transition all 200ms ease */
+	.sidebar-items
+		width 100%
+		height 100vh
+		background-color hsl(0, 0%, 90%)
+		display flex
+		flex-direction column
+		align-items center
+		justify-content flex-end
+		color hsl(0, 0%, 0%)
+		overflow hidden
+		.items-group
+			display flex
+			flex-direction row
+			align-items center
+			justify-content center
+			margin 0 43px 30px 0
+			transition all 200ms ease-out
+			&:hover
+				margin 0 -25px 30px 0
+				.links
+					opacity 1
+				p
+					opacity 0
+			.links
+				opacity 0
+				transition all 200ms linear
+			a
+				display flex
+				align-items center
+				justify-content center
+				width 40px
+				height 40px
+				font-size 20px
+				transition all 200ms ease
+				&:hover
+					background-color hsl(0, 0%, 80%)
+					font-size 23px
+			p
+				opacity 1
+				bold()
+				writing-mode vertical-rl
+				text-orientation mixed
+				transition all 200ms linear
+</style>
