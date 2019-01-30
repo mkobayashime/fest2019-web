eeb128e8a87eefb948ad45eef4841f922bcef959
mkobayashime <m.kobayashi.me@gmail.com>
2019-01-31 01:42:40 +0900

    Delete some unnecessary default codes

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,5 @@
 <template lang="pug">
   section.container
-    div
-      h1.title
-        fest2019-web
-      h2.subtitle
-        Website for 73rd Nada School Festival
 </template>
 
 <script>
@@ -13,5 +8,5 @@ export default {
 }
 </script>
 
-<style lang="stylus">
+<style lang="stylus" scoped>
 </style>
