6a4a580f6f33b188f5b117e15906260f266604c4
mkobayashime <m.kobayashi.me@gmail.com>
2019-02-01 00:39:27 +0900

    Add mouse follower

---
+++ components/TheMouseFollower.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+	#the-mouse-follower
+		#mouse-follower-secondary
+		#mouse-follower-primary
+</template>
+
+<script>
+export default {
+  mounted() {
+    const primary = document.getElementById('mouse-follower-primary')
+    const secondary = document.getElementById('mouse-follower-secondary')
+
+    document.addEventListener('mousemove', e => {
+      primary.setAttribute(
+        'style',
+        `top: ${e.pageY - 8}px; left: ${e.pageX - 8}px;`
+      )
+      secondary.setAttribute(
+        'style',
+        `top: ${e.pageY - 25}px; left: ${e.pageX - 25}px;`
+      )
+    })
+  }
+}
+</script>
+
+
+<style lang="stylus">
+#mouse-follower-primary
+	width 16px
+	height 16px
+	border-radius 50%
+	background-color #2962ff
+	position absolute
+	transition all 10ms ease-out
+	pointer-events none
+#mouse-follower-secondary
+	width 50px
+	height 50px
+	border-radius 50%
+	background-color hsl(0, 0%, 80%)
+	opacity .3
+	position absolute
+	transition all 200ms ease-out
+	pointer-events none
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,13 +3,15 @@
     the-bg
     the-sidebar
     nuxt
+    the-mouse-follower
 </template>
 
 <script>
 export default {
   components: {
     TheBg: () => import('~/components/TheBg.vue'),
-    TheSidebar: () => import('~/components/TheSidebar.vue')
+    TheSidebar: () => import('~/components/TheSidebar.vue'),
+    TheMouseFollower: () => import('~/components/TheMouseFollower.vue')
   }
 }
 </script>
