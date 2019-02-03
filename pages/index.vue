8c108f609a2729247d465da19c99317d836a755a
mkobayashime <m.kobayashi.me@gmail.com>
2019-02-04 01:59:44 +0900

    Add hero/concept section to top page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,24 @@
 <template lang="pug">
-  section.container
+  .index
+    .hero
+      h1
+        img(src="~/assets/logo/black/tagline-jp.svg" alt="第73回灘校文化祭")
+      h2
+        img(src="~/assets/logo/black/logotype.svg" alt="SAIL AWAY")
+      p.date 2018.05.02&ndash;03
+      .button-container
+        nuxt-link.button(to="/about")
+          span 開催概要
+    .concept
+      img.logo(src="~/assets/logo/black/vertical-b.svg")
+      .paragraph
+        p 文化祭を航海になぞらえたこのテーマは、
+        p 世界という大海へ懸命に漕ぎ出す灘校生と、
+        p その出発点となるコミュニティとしての灘校を表しています
+        p 停泊していた船は、動き始めるときにいかりを上げます
+        p 水面から上がる瞬間のいかりを切り取ったロゴは、
+        p 文化祭をきっかけとして始まる、
+        p 壮大な航海を暗示しています
 </template>
 
 <script>
@@ -9,4 +28,40 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+.index
+  width 1000px
+  margin 0 auto
+  .hero
+    height 80vh
+    padding-top 250px
+    h1
+      width 50%
+      margin 0 auto
+    h2
+      width 85%
+      margin 30px auto 0
+    .date
+      margin 50px auto 0
+      text-align center
+      font-size 30px
+      bold()
+    .button-container
+      margin 50px auto
+      display flex
+      justify-content center
+      .button
+        btn-primary()
+  .concept
+    height 90vh
+    display flex
+    flex-direction row
+    align-items center
+    justify-content center
+    .logo
+      width 30%
+      margin 0 100px
+    p
+      bold()
+      font-size 16px
+      margin-top 20px
 </style>
