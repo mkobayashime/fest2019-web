df5b153a13932fe676d17972e918b8e36d86e2e3
mkobayashime <m.kobayashi.me@gmail.com>
2019-03-01 14:04:25 +0900

    Extend the area of hover effect

--- components/TheSidebar.vue
+++ components/TheSidebar.vue
@@ -2,22 +2,24 @@
 	#the-sidebar
 		the-menu-btn
 		.sidebar-items
-			.items-group
-				.links
-					a(:href="sns.twitter" target="_blank")
-						.sidebar-item.fab.fa-twitter
-					a(:href="sns.ig" target="_blank")
-						.sidebar-item.fab.fa-instagram
-					a(:href="sns.fb" target="_blank")
-						.sidebar-item.fab.fa-facebook
-				p Follow Us
-			.items-group
-				.links
-					a(:href="sns.twitterShare" target="_blank")
-						.sidebar-item.fab.fa-twitter
-					a(:href="sns.fbShare" target="_blank")
-						.sidebar-item.fab.fa-facebook
-				p Share On
+			.items-container
+				.items-group
+					.links
+						a(:href="sns.twitter" target="_blank")
+							.sidebar-item.fab.fa-twitter
+						a(:href="sns.ig" target="_blank")
+							.sidebar-item.fab.fa-instagram
+						a(:href="sns.fb" target="_blank")
+							.sidebar-item.fab.fa-facebook
+					p Follow Us
+			.items-container
+				.items-group
+					.links
+						a(:href="sns.twitterShare" target="_blank")
+							.sidebar-item.fab.fa-twitter
+						a(:href="sns.fbShare" target="_blank")
+							.sidebar-item.fab.fa-facebook
+					p Share On
 </template>
 
 <script>
@@ -47,39 +49,43 @@ export default {
 		flex-direction column
 		align-items center
 		justify-content flex-end
+		padding-bottom 20px
 		color hsl(0, 0%, 0%)
 		overflow hidden
-		.items-group
-			display flex
-			flex-direction row
-			align-items center
-			justify-content center
-			margin -15px 43px 40px 0
-			transition all 200ms ease-out
-			&:hover
-				margin -15px -25px 40px 0
+		.items-container
+			width 100%
+			overflow hidden
+			&:hover .items-group
+				margin 0px -25px 10px 0
 				.links
 					opacity 1
 				p
 					opacity 0
-			.links
-				opacity 0
-				transition all 200ms linear
-			a
+			.items-group
 				display flex
+				flex-direction row
 				align-items center
 				justify-content center
-				width 40px
-				height 40px
-				font-size 20px
-				transition all 200ms ease
-				&:hover
-					background-color hsl(0, 0%, 80%)
-					font-size 23px
-			p
-				opacity 1
-				bold()
-				writing-mode vertical-rl
-				text-orientation mixed
-				transition all 200ms linear
+				margin 0px 43px 10px 0
+				transition all 200ms ease-out
+				.links
+					opacity 0
+					transition all 200ms linear
+				a
+					display flex
+					align-items center
+					justify-content center
+					width 40px
+					height 40px
+					font-size 20px
+					transition all 200ms ease
+					&:hover
+						background-color hsl(0, 0%, 80%)
+						font-size 23px
+				p
+					opacity 1
+					bold()
+					writing-mode vertical-rl
+					text-orientation mixed
+					transition all 200ms linear
 </style>
