f98231dce509e573d269938a97ef80800e64888c
mkobayashime <m.kobayashi.me@gmail.com>
2019-03-03 01:00:18 +0900

    Add a plugin for Youtube embedding

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,4 @@
 const resolve = require('path').resolve
-const pkg = require('./package')
 
 module.exports = {
   mode: 'spa',
@@ -48,7 +47,7 @@ module.exports = {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: ['~/plugins/youtube.js'],
 
   /*
    ** Router config
---
+++ plugins/youtube.js
@@ -0,0 +1,3 @@
+import Vue from 'vue'
+import VueYoutube from 'vue-youtube'
+Vue.use(VueYoutube)
