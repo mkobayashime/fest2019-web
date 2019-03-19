87c6b0f03c06a19ba1c774fc3680190a0bba237d
mkobayashime <m.kobayashi.me@gmail.com>
2019-03-19 19:27:11 +0900

    Add dummy text to some pages

--- pages/about.vue
+++ pages/about.vue
@@ -1,4 +1,6 @@
 <template lang="pug">
+  #about
+    p.sorry 現在準備中です...( *・ω・)*_ _))
 </template>
 
 <script>
@@ -9,4 +11,17 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+#about
+  width 100%
+  height 100%
+  top 0
+  left 0
+  display flex
+  justify-content center
+  align-items center
+  .sorry
+    bold()
+    font-size 2rem
+    letter-spacing .05em
+</style>
---
+++ pages/design/index.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  #about
+    p.sorry 現在準備中です...( *・ω・)*_ _))
+</template>
+
+<script>
+export default {
+  head: {
+    title: '文化委員会について'
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+#about
+  width 100%
+  height 100%
+  top 0
+  left 0
+  display flex
+  justify-content center
+  align-items center
+  .sorry
+    bold()
+    font-size 2rem
+    letter-spacing .05em
+</style>
