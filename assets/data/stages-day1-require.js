c6b5b354b3559358fa79689450d38c4d727badab
mkobayashime <m.kobayashi.me@gmail.com>
2019-04-28 19:08:20 +0900

    Add schedule of events in stages

---
+++ assets/data/stages-day1-require.js
@@ -0,0 +1,7 @@
+import centerStage from '~/assets/timetable/stages/day1/centerStage.json'
+import audioVisual from '~/assets/timetable/stages/day1/audioVisual.json'
+import lectureRoom from '~/assets/timetable/stages/day1/lectureRoom.json'
+import gym from '~/assets/timetable/stages/day1/gym.json'
+import auditorium from '~/assets/timetable/stages/day1/auditorium.json'
+import musicRoom from '~/assets/timetable/stages/day1/musicRoom.json'
+export { centerStage, audioVisual, lectureRoom, gym, auditorium, musicRoom }
